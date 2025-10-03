c975d19a9b3e6e0b93a1b9cb88c87dd32daa7043
Arjun Gole <31399532+arjungole@users.noreply.github.com>
2025-10-03 18:26:45 +0530

    changes to work with node 14 and below index.cjs.js

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -155,7 +155,7 @@ const reverseNames = Object.create(null);
 
 // Create a list of reverse color names
 for (const name in cssKeywords) {
-	if (Object.hasOwn(cssKeywords, name)) {
+	if (Object.prototype.hasOwnProperty.call(cssKeywords, name)) {
 		reverseNames[cssKeywords[name]] = name;
 	}
 }
@@ -257,7 +257,7 @@ cs.get.rgb = function (string) {
 			return [0, 0, 0, 0];
 		}
 
-		if (!Object.hasOwn(cssKeywords, match[1])) {
+		if (!!Object.prototype.hasOwnProperty.call(cssKeywords, match[1])) {
 			return null;
 		}
 
